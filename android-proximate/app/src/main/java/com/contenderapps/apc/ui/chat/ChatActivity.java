bb4e1c7bddf569aa0ba12f7b1c5d57dbd6292082
Pavel Samokha <vansickle@gmail.com>
2018-10-21 00:29:33 +0200

    moved to fragment

--- android-proximate/app/src/main/java/com/contenderapps/apc/ui/chat/ChatActivity.java
+++ android-proximate/app/src/main/java/com/contenderapps/apc/ui/chat/ChatActivity.java
@@ -53,66 +53,19 @@ import java.util.Locale;
 
 
 public class ChatActivity extends BaseToolbarActivity
-        implements
-// MessagesListAdapter.SelectionListener,
-    MessagesListAdapter.OnLoadMoreListener,
-        MessageInput.InputListener,
-        MessageInput.AttachmentsListener
 {
-
-        private static final int TOTAL_MESSAGES_COUNT = 100;
-
-        protected final String senderId = "0";
-        protected ImageLoader imageLoader;
-        protected MessagesListAdapter<Message> messagesAdapter;
-
-        private Menu menu;
-        private int selectionCount;
-        private Date lastLoadedDate;
-
-        private MessagesList messagesList;
-
         @Override
         protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
             setToolbarTitle("Chat");
 
-//            if (savedInstanceState == null) {
-//                replaceFragment(ChatFragment.newInstance());
-//            }
-
-
-            imageLoader = new ImageLoader() {
-                @Override
-                public void loadImage(ImageView imageView, String url, Object payload) {
-                    Picasso.with(ChatActivity.this).load(url).into(imageView);
-                }
-            };
-
-            setContentView(R.layout.activity_chat);
-
-            this.messagesList = (MessagesList) findViewById(R.id.messagesList);
-            initAdapter();
-
-            MessageInput input = (MessageInput) findViewById(R.id.input);
-            input.setInputListener(this);
-            input.setAttachmentsListener(this);
-
+            if (savedInstanceState == null) {
+                replaceFragment(ChatFragment.newInstance());
+            }
         }
 
-        @Override
-        protected void onStart() {
-            super.onStart();
-            messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(0), "I want you to receive for me \"Striped Hudson Jeans Shorts\" from Mercari. " +
-                    "Expected on Monday, October 22"), true);
-            Message message = new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(0), "");
-            message.setImage(new Message.Image(MessagesFixtures.getImage(0)));
-            messagesAdapter.addToStart(message, true);
-            messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(1), "Ready to receive it for you. My address Nagodziców 18-54."), true);
 
-//            messagesAdapter.addToStart(MessagesFixtures.getTextMessage(), true);
-        }
 
 //        @Override
 //        public boolean onCreateOptionsMenu(Menu menu) {
@@ -136,22 +89,15 @@ public class ChatActivity extends BaseToolbarActivity
 //            return true;
 //        }
 
-        @Override
-        public void onBackPressed() {
-            if (selectionCount == 0) {
-                super.onBackPressed();
-            } else {
-                messagesAdapter.unselectAllItems();
-            }
-        }
-
-        @Override
-        public void onLoadMore(int page, int totalItemsCount) {
-            Log.i("TAG", "onLoadMore: " + page + " " + totalItemsCount);
-//            if (totalItemsCount < TOTAL_MESSAGES_COUNT) {
-//                loadMessages();
+//        @Override
+//        public void onBackPressed() {
+//            if (selectionCount == 0) {
+//                super.onBackPressed();
+//            } else {
+//                messagesAdapter.unselectAllItems();
 //            }
-        }
+//        }
+
 
 //        @Override
 //        public void onSelectionChanged(int count) {
@@ -160,16 +106,16 @@ public class ChatActivity extends BaseToolbarActivity
 //            menu.findItem(R.id.action_copy).setVisible(count > 0);
 //        }
 
-        protected void loadMessages() {
-            new Handler().postDelayed(new Runnable() { //imitation of internet connection
-                @Override
-                public void run() {
-                    ArrayList<Message> messages = MessagesFixtures.getMessages(lastLoadedDate);
-                    lastLoadedDate = messages.get(messages.size() - 1).getCreatedAt();
-                    messagesAdapter.addToEnd(messages, false);
-                }
-            }, 1000);
-        }
+//        protected void loadMessages() {
+//            new Handler().postDelayed(new Runnable() { //imitation of internet connection
+//                @Override
+//                public void run() {
+//                    ArrayList<Message> messages = MessagesFixtures.getMessages(lastLoadedDate);
+//                    lastLoadedDate = messages.get(messages.size() - 1).getCreatedAt();
+//                    messagesAdapter.addToEnd(messages, false);
+//                }
+//            }, 1000);
+//        }
 
         private MessagesListAdapter.Formatter<Message> getMessageStringFormatter() {
             return new MessagesListAdapter.Formatter<Message>() {
@@ -186,55 +132,6 @@ public class ChatActivity extends BaseToolbarActivity
                 }
             };
         }
-
-    @Override
-    public boolean onSubmit(CharSequence input) {
-        Message message = new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(0), input.toString());
-        messagesAdapter.addToStart(message, true);
-
-        new Handler().postDelayed(new Runnable() { //imitation of internet connection
-            @Override
-            public void run() {
-                messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(1), "Great. I'll write to you when receive it"), true);
-
-            }
-        }, 2000);
-
-
-        new Handler().postDelayed(new Runnable() { //imitation of internet connection
-            @Override
-            public void run() {
-                messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(1), "Received your package"), true);
-
-            }
-        }, 10000);
-
-        return true;
-    }
-
-
-    @Override
-    public void onAddAttachments() {
-//        new AlertDialog.Builder(this)
-//                .setItems(R.array.view_types_dialog, this)
-//                .show();
-    }
-
-    private void initAdapter() {
-        messagesAdapter = new MessagesListAdapter<>(senderId, imageLoader);
-//        messagesAdapter.enableSelectionMode(this);
-        messagesAdapter.setLoadMoreListener(this);
-//        messagesAdapter.registerViewClickListener(R.id.messageUserAvatar,
-//                new MessagesListAdapter.OnMessageViewClickListener<Message>() {
-//                    @Override
-//                    public void onMessageViewClick(View view, Message message) {
-//                        AppUtils.showToast(DefaultMessagesActivity.this,
-//                                message.getUser().getName() + " avatar click",
-//                                false);
-//                    }
-//                });
-        this.messagesList.setAdapter(messagesAdapter);
-    }
 
     private static final String TAG = ChatActivity.class.getSimpleName();
 
--- android-proximate/app/src/main/java/com/contenderapps/apc/ui/chat/ChatFragment.java
+++ android-proximate/app/src/main/java/com/contenderapps/apc/ui/chat/ChatFragment.java
@@ -2,17 +2,33 @@ package com.contenderapps.apc.ui.chat;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.contenderapps.apc.R;
+import com.contenderapps.apc.data.chat.Message;
+import com.contenderapps.apc.data.fixtures.MessagesFixtures;
 import com.contenderapps.apc.ui.base.fragments.BaseMvpFragment;
+import com.squareup.picasso.Picasso;
+import com.stfalcon.chatkit.commons.ImageLoader;
+import com.stfalcon.chatkit.messages.MessageInput;
+import com.stfalcon.chatkit.messages.MessagesList;
+import com.stfalcon.chatkit.messages.MessagesListAdapter;
+
+import java.util.Date;
 
 
-public class ChatFragment extends BaseMvpFragment<ChatMvpView, ChatPresenter> implements ChatMvpView {
+public class ChatFragment extends BaseMvpFragment<ChatMvpView, ChatPresenter> implements ChatMvpView,
+    MessageInput.InputListener,
+    MessageInput.AttachmentsListener
+{
 
     private static final String TAG = ChatFragment.class.getSimpleName();
 
@@ -21,10 +37,28 @@ public class ChatFragment extends BaseMvpFragment<ChatMvpView, ChatPresenter> im
         return fragment;
     }
 
+    private static final int TOTAL_MESSAGES_COUNT = 100;
+
+    protected final String senderId = "0";
+    protected ImageLoader imageLoader;
+    protected MessagesListAdapter<Message> messagesAdapter;
+
+    private Menu menu;
+    private int selectionCount;
+    private Date lastLoadedDate;
+
+    private MessagesList messagesList;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        imageLoader = new ImageLoader() {
+            @Override
+            public void loadImage(ImageView imageView, String url, Object payload) {
+                Picasso.with(getActivity()).load(url).into(imageView);
+            }
+        };
     }
 
     @Override
@@ -48,6 +82,14 @@ public class ChatFragment extends BaseMvpFragment<ChatMvpView, ChatPresenter> im
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        this.messagesList = (MessagesList) view.findViewById(R.id.messagesList);
+        initAdapter();
+
+        MessageInput input = (MessageInput) view.findViewById(R.id.input);
+        input.setInputListener(this);
+        input.setAttachmentsListener(this);
+
         return view;
     }
 
@@ -89,7 +131,67 @@ public class ChatFragment extends BaseMvpFragment<ChatMvpView, ChatPresenter> im
 //        mPresenter.loadData();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(0), "I want you to receive for me \"Striped Hudson Jeans Shorts\" from Mercari. " +
+                "Expected on Monday, October 22"), true);
+        Message message = new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(0), "");
+        message.setImage(new Message.Image(MessagesFixtures.getImage(0)));
+        messagesAdapter.addToStart(message, true);
+        messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(1), "Ready to receive it for you. My address Nagodziców 18-54."), true);
 
+//            messagesAdapter.addToStart(MessagesFixtures.getTextMessage(), true);
+    }
+
+    @Override
+    public boolean onSubmit(CharSequence input) {
+        Message message = new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(0), input.toString());
+        messagesAdapter.addToStart(message, true);
+
+        new Handler().postDelayed(new Runnable() { //imitation of internet connection
+            @Override
+            public void run() {
+                messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(1), "Great. I'll write to you when receive it"), true);
+
+            }
+        }, 2000);
+
+
+        new Handler().postDelayed(new Runnable() { //imitation of internet connection
+            @Override
+            public void run() {
+                messagesAdapter.addToStart(new Message(MessagesFixtures.getRandomId(), MessagesFixtures.getUser(1), "Received your package"), true);
+
+            }
+        }, 10000);
+
+        return true;
+    }
+
+
+    @Override
+    public void onAddAttachments() {
+//        new AlertDialog.Builder(this)
+//                .setItems(R.array.view_types_dialog, this)
+//                .show();
+    }
+
+    private void initAdapter() {
+        messagesAdapter = new MessagesListAdapter<>(senderId, imageLoader);
+//        messagesAdapter.enableSelectionMode(this);
+//        messagesAdapter.setLoadMoreListener(this);
+//        messagesAdapter.registerViewClickListener(R.id.messageUserAvatar,
+//                new MessagesListAdapter.OnMessageViewClickListener<Message>() {
+//                    @Override
+//                    public void onMessageViewClick(View view, Message message) {
+//                        AppUtils.showToast(DefaultMessagesActivity.this,
+//                                message.getUser().getName() + " avatar click",
+//                                false);
+//                    }
+//                });
+        this.messagesList.setAdapter(messagesAdapter);
+    }
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
